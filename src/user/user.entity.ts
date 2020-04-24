29b7fb363840ea807c62282fdca2c4b0223639d0
halilibrahim <h.ibrahim.erbas@gmail.com>
2020-04-24 17:09:47 +0300

    swagger-api-property

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,19 +1,25 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
 export class UserEntity {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
-  @Column()
+  @Column({ type: "varchar", length: 200, nullable: false })
+  @ApiProperty()
   firstname: string;
 
-  @Column()
+  @Column({ type: "varchar", length: 200 })
+  @ApiProperty()
   lastname: string;
 
-  @Column()
+  @Column({ type: "varchar", unique: true, length: 200 })
+  @ApiProperty()
   email: string;
 
-  @Column()
+  @Column({ type: "varchar", nullable: false, length: 70 })
+  @ApiProperty()
   password: string;
 }
